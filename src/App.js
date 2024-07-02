5ad4b2d475209e0617d4700fa0aab17b0c8d5a39
priyansh70 <priyansh70890@gmail.com>
2024-07-03 00:59:32 +0530

    Prop drilling done in Main Component By Component Composition

--- src/App.js
+++ src/App.js
@@ -59,7 +59,12 @@ export default function App() {
         <Search />
         <NumResults movies={movies} />
       </Navbar>
-      <Main movies={movies} />
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
@@ -103,16 +108,11 @@ function NumResults({ movies }) {
   );
 }
 
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -122,7 +122,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "X" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
@@ -215,7 +215,10 @@ function WatchedMovieList({ watched }) {
   return (
     <ul className="list">
       {watched.map((movie) => (
-        <WatchedMovie movie={movie} />
+        <WatchedMovie
+          movie={movie}
+          key={watched.imdbID}
+        />
       ))}
     </ul>
   );
@@ -223,7 +226,7 @@ function WatchedMovieList({ watched }) {
 
 function WatchedMovie({ movie }) {
   return (
-    <li key={movie.imdbID}>
+    <li>
       <img
         src={movie.Poster}
         alt={`${movie.Title} poster`}
